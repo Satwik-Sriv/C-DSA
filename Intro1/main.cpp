726348bb14eb247617ad720db6d628ebd8884ce2
satwik <satwiksrivastava.994@gmail.com>
2025-05-27 14:04:57 +0530

    print methods for terminal

---
+++ Intro1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    std::cout << "Satwik Srivastava" << std::endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    int result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
+}
