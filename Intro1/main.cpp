1ead2920a4d1e9217f958b8a593e14b2decb6e77
satwik <satwiksrivastava.994@gmail.com>
2025-05-27 17:45:00 +0530

    use comments

--- Intro1/main.cpp
+++ Intro1/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
+// This is a single line comment
+
+/* Yes,
+ This a multi line comment!
+Would be pretty handy.abort
+ Block comments cannot be nested
+ // This is a comment which can be nested in.
+*/
+
 int main(){
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
-    std::cout << "Satwik Srivastava" << std::endl;
     std::cout << "Satwik Srivastava" << std::endl;
     return 0;
 }
